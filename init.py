8e7843b7b9e6548a1e40b65603bb9e588e722cae
maticaputti <matias.caputti@gmail.com>
2018-07-13 13:10:29 -0300

    Update

--- init.py
+++ init.py
@@ -22,13 +22,21 @@ while(True):
 
     # Add rectable to original frame
     cv2.rectangle(frame, (LEFT, TOP), (RIGHT, BOTTOM), (0,255,0), 2)
+    LABEL_TEXT = 'Test label'
+    LABEL_COLOR = (255,255,255)
+    cv2.putText(frame, LABEL_TEXT, (LEFT, TOP-7), cv2.FONT_HERSHEY_SIMPLEX, 0.45, LABEL_COLOR, 2)
     cv2.imshow("Frame", frame)
 
     # Cut ROI
     roi = frame[TOP+2:BOTTOM-2, LEFT+2:RIGHT-2]
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (7, 7), 0)
-    cv2.imshow("ROI", gray)
+    # cv2.imshow("ROI", gray)
+
+    # detector = cv2.CascadeClassifier()
+    # rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5,
+    #                                 minSize=(30,30),
+    #                                 flags=cv2.CASCADE_SCALE_IMAGE)
 
     # if the user pressed "q", then stop looping
     if cv2.waitKey(1) & 0xFF == ord("q"):
