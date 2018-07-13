ce3e33e00be4a5d57d8af5ddb7b0130b67012399
maticaputti <matias.caputti@gmail.com>
2018-07-13 14:53:11 -0300

    Update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Real-time digit classification
+
+Real-time digit classification using MNIST handwritten digit database OpenCV and Python.
+
+<img width="1418" src="https://user-images.githubusercontent.com/7065401/42706161-3fa625d2-86ac-11e8-9f67-a003d92898e2.png">
--- init.py
+++ init.py
@@ -1,42 +1,44 @@
 import cv2
 import imutils
 import numpy as np
-from matplotlib import pyplot as plt
+from keras.models import load_model
 
 # get the reference to the webcam
 CAMERA = cv2.VideoCapture(0)
 
-CAPTURE_WIDTH = 700
-ROI_LONG = 200 # Region Of Interest
+CAPTURE_WIDTH = 900
+ROI_LONG = 400 # Region Of Interest
 MARGIN = 50
 TOP = MARGIN
 RIGHT = CAPTURE_WIDTH - MARGIN
 BOTTOM = TOP + ROI_LONG
 LEFT = RIGHT - ROI_LONG
 
+model = load_model('MNIST_model.h5')
+
 while(True):
     _, frame = CAMERA.read()
     frame = imutils.resize(frame, CAPTURE_WIDTH)
-    frame = cv2.flip(frame, 1)
+    # frame = cv2.flip(frame, 1)
     (height, width) = frame.shape[:2]
 
     # Add rectable to original frame
     cv2.rectangle(frame, (LEFT, TOP), (RIGHT, BOTTOM), (0,255,0), 2)
-    LABEL_TEXT = 'Test label'
-    LABEL_COLOR = (255,255,255)
-    cv2.putText(frame, LABEL_TEXT, (LEFT, TOP-7), cv2.FONT_HERSHEY_SIMPLEX, 0.45, LABEL_COLOR, 2)
-    cv2.imshow("Frame", frame)
 
-    # Cut ROI
+    # Cut ROI and preprocess
     roi = frame[TOP+2:BOTTOM-2, LEFT+2:RIGHT-2]
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (7, 7), 0)
-    # cv2.imshow("ROI", gray)
+    _, gray = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV) # need fixes
+    cv2.imshow("ROI", gray)
 
-    # detector = cv2.CascadeClassifier()
-    # rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5,
-    #                                 minSize=(30,30),
-    #                                 flags=cv2.CASCADE_SCALE_IMAGE)
+    # Predict and show prediction
+    gray_small = imutils.resize(gray, 28)
+    gray_small = gray_small.reshape(1,28,28,1)
+    pred = model.predict_classes(gray_small)[0]
+    LABEL_TEXT = str(pred)
+    LABEL_COLOR = (0,255,0)
+    cv2.putText(frame, LABEL_TEXT, (LEFT, TOP-7), cv2.FONT_HERSHEY_SIMPLEX, 1, LABEL_COLOR, 2)
+    cv2.imshow("Frame", frame)
 
     # if the user pressed "q", then stop looping
     if cv2.waitKey(1) & 0xFF == ord("q"):
